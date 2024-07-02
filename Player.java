268fa5663c018c877ffe8b68fd0a56ae9b20079a
beksus <85835904+beksus@users.noreply.github.com>
2024-07-03 01:23:27 +0800

    Player class created and different only in constructor compared to the main

---
+++ Player.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Player {
+    private String username;
+    private int score;
+    private ArrayList<String> pokemon_list;
+
+
+    //constructor
+    public Player() {
+        this.username = "";
+        this.score = 0;
+        this.pokemon_list = new ArrayList<String>(4);
+    }
+
+
+    //getters
+    public String getUsername() {
+        return username;
+    }
+
+    public int getScore() {
+        return score;
+    }
+    public ArrayList<String> getPokemon_list() {
+        return pokemon_list;
+    }
+
+
+    //setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setPokemon_list(ArrayList<String> pokemon_list) {
+        this.pokemon_list = pokemon_list;
+    }
+
+}
