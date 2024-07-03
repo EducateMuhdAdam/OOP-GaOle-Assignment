a88f0ff18c466e629df4e5fd608375a7e6fa0574
jinhusky <123972541+jinhusky@users.noreply.github.com>
2024-07-03 22:27:36 +0800

    beta 1 still testing stuff out
    figuring out the logic flow of the game menu etc

--- gameMaster.java
+++ gameMaster.java
@@ -1,1 +1,42 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.Duration;
+import java.util.Scanner;
 
+public class gameMaster {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		int userChoice;
+
+		userChoice = displayMenu(input);	
+		switch(userChoice) {
+			case 1:
+				//LocalDateTime start = LocalDateTime.now();
+				//format the currentTime object to a readable string
+				//DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+				//LocalDateTime end = LocalDateTime.now();
+				break;
+				
+			case 2:
+				System.out.println("2. Options");
+				break;
+			case 3:
+				System.out.println("3. Quit");
+				break;
+			}
+		input.close();
+	}
+		
+			
+	private static int displayMenu(Scanner input) {
+	
+		System.out.println("-------------------");
+		System.out.printf("|1) PLAY%10s|\n","");
+		System.out.printf("|2) OPTIONS%7s|\n","");
+		System.out.printf("|1) QUIT%10s|\n","");
+		System.out.println("-------------------");
+		System.out.println("Make a selection: ");
+		
+		return input.nextInt();
+	}	
+}
