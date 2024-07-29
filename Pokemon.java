1f39d93e6e9e995f3ae7a32e57878d477538b387
Beks <85835904+beksus@users.noreply.github.com>
2024-07-30 05:38:27 +0800

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,93 +1,60 @@
-import java.io.*;
-import java.util.*;
-
 public class Pokemon {
     private int pokemon_id;
     private String name;
-    private ArrayList<Type> type;
+    private String[] type;
     private Team team;
-    private int grade;
-    
     private int hp_max;
     private int attack;
     private int defense;
-    private int speed;
-    
     private int current_hp;
-    private int stage_attack;
-    private int stage_defense;
-    private int stage_speed;
+    private int stage;
     
+    private int current_attack;
+    private int current_defense;
+    private int max_speed;
+    private int current_speed;
     private Status status_effect;
     private Moves move1;
     private Moves move2;
-    
-    public Pokemon(int pokemon_id, Team team){
+
+    private boolean isGen2OrLater;
+
+    public Pokemon(int pokemon_id, String name, String[] type, int attack, int current_attack, int defense, int max_speed, int hp_max, String moveID1, String moveID2) {
         this.pokemon_id = pokemon_id;
-        this.team = team;
-        this.status_effect = new Status(this);
-        
-        ArrayList<String[]> pokemon_data = new ArrayList<String[]>();
-		
-		try {
-			File textfile = new File(System.getProperty("user.dir") + "\\src\\Set01.csv");
-			Scanner data = new Scanner(textfile);
-			while (data.hasNextLine()) {
-				pokemon_data.add(data.nextLine().split(",", 0));
-			}
-			data.close();
-		}
-		catch (FileNotFoundException e) {
-			System.out.println("Pokemon File Can't Be Read");
-		}
-        for (String[] pokemonlog : pokemon_data) {
-			if (pokemonlog[0].equals(Integer.toString(pokemon_id))) {
-				name = pokemonlog[1];
-				type = new ArrayList<Type>();
-				for (String t: pokemonlog[2].split("/", 0)) {
-					type.add(InitType(t));
-				}
-				grade = Integer.parseInt(pokemonlog[3]);
-				hp_max = Integer.parseInt(pokemonlog[4]);
-				current_hp = Integer.parseInt(pokemonlog[4]);
-				attack = Integer.parseInt(pokemonlog[5]);
-                defense = Integer.parseInt(pokemonlog[6]);
-                speed = Integer.parseInt(pokemonlog[7]);
-                move1 = new Moves(this, pokemonlog[8]); 
-			}
-		}
-        stage_attack = 0;
-        stage_defense = 0;
-        stage_speed = 0;
-		
+        this.name = name;
+        this.type = type;
+        this.current_hp = hp_max;
+        this.attack = attack;
+        this.current_attack = current_attack;
+        this.defense = defense;
+        this.current_defense = defense;
+        this.max_speed = max_speed;
+        this.current_speed = max_speed;
+        this.hp_max = hp_max;
+        move1 = new Moves(this, moveID1);
+        move2 = new Moves(this, moveID2);
+        this.isGen2OrLater = false;
+        this.stage = 0;
+
     }
     
-    private Type InitType(String type) {
-		switch (type.toLowerCase()){
-		case "normal": return Type.Normal;
-		case "fire": return Type.Fire;
-		case "water": return Type.Water;
-		case "grass": return Type.Grass;
-		case "electric": return Type.Electric;
-		case "ice": return Type.Ice;
-		case "fighting": return Type.Fighting;
-		case "poison": return Type.Poison;
-		case "ground": return Type.Ground;
-		case "flying": return Type.Flying;
-		case "psychic": return Type.Psychic;
-		case "bug": return Type.Bug;
-		case "rock": return Type.Rock;
-		case "ghost": return Type.Ghost;
-		case "dragon": return Type.Dragon;
-		case "dark": return Type.Dark;
-		case "steel": return Type.Steel;
-		case "fairy": return Type.Fairy;
-		default: {
-			System.out.println("Type Error, Defaulting to Normal");
-			return Type.Normal;
-		}
-		}
-	}
+    public Pokemon(int pokemon_id, String moveID1, String moveID2, Team team) {
+    	this.pokemon_id = pokemon_id;
+    	this.name = "Pikachu";
+    	this.current_hp = 100;
+        this.attack = 50;
+        this.current_attack = 50;
+        this.defense = 50;
+        this.current_defense = 50;
+        this.max_speed = 50;
+        this.current_speed = 50;
+        this.hp_max = 100;
+        this.team = team;
+        move1 = new Moves(this, moveID1);
+        move2 = new Moves(this, moveID2);
+        status_effect = new Status(this);
+    }
+
 
 	
     public int getPokemon_id() {
@@ -96,7 +63,7 @@ public class Pokemon {
     public String getName() {
         return this.name;
     }
-    public ArrayList<Type> getType() {
+    public String[] getType() {
         return this.type;
     }
     public Team getTeam() {
@@ -105,8 +72,8 @@ public class Pokemon {
     public int getCurrent_hp() {
         return this.current_hp;
     }
-    public int getCurrent_attack() {
-    	return attack;
+    public double getCurrent_attack() {
+    	return this.current_attack * this.calculateStatMultiplier(this.getStage(), getIsGen2OrLater());
     }
     public int getAttack() {
         return this.attack;
@@ -114,14 +81,14 @@ public class Pokemon {
     public int getDefense() {
         return this.defense;
     }
-    public int getCurrent_defense() {
-        return this.defense;
+    public double getCurrent_defense() {
+        return this.current_defense * this.calculateStatMultiplier(this.getStage(), getIsGen2OrLater());
     }
     public int getMax_speed() {
-        return this.speed;
+        return this.max_speed;
     }
-    public int getCurrent_speed() {
-        return this.speed;
+    public double getCurrent_speed() {
+        return this.current_speed * this.calculateStatMultiplier(this.getStage(), getIsGen2OrLater());
     }
     public int getHp_max() {
         return this.hp_max;
@@ -136,9 +103,21 @@ public class Pokemon {
     	return status_effect;
     }
     
+    public int getStage() {
+    	return stage;
+    }
+    public void setIsGen2OrLater(boolean isGen2OrLater) {
+        this.isGen2OrLater = isGen2OrLater;
+    }
 
 
-    public void setType(ArrayList<Type> type) {
+    public void setStage(int stage) {
+    	this.stage = stage;
+    }
+    public boolean getIsGen2OrLater() {
+        return this.isGen2OrLater;
+    }
+    public void setType(String[] type) {
         this.type = type;
     }
     public void setCurrent_hp(int current_hp) {
@@ -146,23 +125,23 @@ public class Pokemon {
     }
 
 
-    public void setCurrent_defense(int stage_defense) {
-        this.stage_defense = stage_defense;
+    public void setCurrent_defense(int current_defense) {
+        this.current_defense = current_defense;
     }
 
-    public void setCurrent_speed(int stage_speed) {
-        this.stage_speed = stage_speed;
+    public void setCurrent_speed(int current_speed) {
+        this.current_speed = current_speed;
     }  
     public void changeSpeed(int speed) {
-    	stage_speed += speed;
+    	current_speed += speed;
     }
     
     public void changeAttack(int atk) {
-    	stage_attack += atk;
+    	current_attack += atk;
     }
     
     public void changeDefence(int def) {
-    	stage_attack += def;
+    	current_attack += def;
     }
     
     public void takeDamage(int damage) {
@@ -173,11 +152,43 @@ public class Pokemon {
 	@Override
 	public String toString() {
 		return "Pokemon [pokemon_id=" + pokemon_id + ", name=" + name + ", hp_max=" + hp_max + ", attack=" + attack
-				+ ", defense=" + defense + ", current_hp=" + current_hp + ", current_attack=" + stage_attack
-				+ ", current_defense=" + stage_defense + ", max_speed=" + speed + ", current_speed="
-				+ stage_speed + "]";
+				+ ", defense=" + defense + ", current_hp=" + current_hp + ", current_attack=" + current_attack
+				+ ", current_defense=" + current_defense + ", max_speed=" + max_speed + ", current_speed="
+				+ current_speed + "]";
 	}
-    
+
+
+    public static double calculateStatMultiplier(int stage, boolean isGen2OrLater) {
+        if (stage < -6 || stage > 6) {
+            throw new IllegalArgumentException("Stage must be between -6 and 6");
+        }
+        if (isGen2OrLater) {
+            return stage >= 0 ? (2.0 + stage) / 2.0 : 2.0 / (2.0 - stage);
+        } else {
+            switch (stage) {
+                case -6: return 0.25;
+                case -5: return 0.28;
+                case -4: return 0.33;
+                case -3: return 0.40;
+                case -2: return 0.50;
+                case -1: return 0.66;
+                case 0: return 1.0;
+                case 1: return 1.5;
+                case 2: return 2.0;
+                case 3: return 2.5;
+                case 4: return 3.0;
+                case 5: return 3.5;
+                case 6: return 4.0;
+                default: throw new IllegalArgumentException("Stage must be between -6 and 6");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        System.out.println(calculateStatMultiplier(-6, true));  // Output: 0.25
+        System.out.println(calculateStatMultiplier(3, false));  // Output: 2.5
+    }
 
 }
 
